fd3a2a49235bbc356b9983bee56cc1dbf8ae86ff
Huenyeon <alhenajulienne.gedalanga-21@cpu.edu.ph>
2025-05-16 21:22:04 +0800

    added e2e for removing all done todos

--- e2e/tests/remove-all-todo.spec.ts
+++ e2e/tests/remove-all-todo.spec.ts
@@ -81,4 +81,83 @@ test.describe("remove all done todos", () => {
 
     await expect(page.locator(".task")).toHaveCount(0);
   });
+
+  test("should be able to remove all done to do even in active todo tab", async({page})=> {
+    await page.locator(".input").fill("First task to mark as done");
+    await page.keyboard.press("Enter");
+    const task1 = page.locator(".task", {
+      hasText: "First task to mark as done",
+    });
+    await expect(task1).toBeVisible();
+
+    await page.locator(".input").fill("Second task to mark as done");
+    await page.keyboard.press("Enter");
+    const task2 = page.locator(".task", {
+      hasText: "Second task to mark as done",
+    });
+    await expect(task2).toBeVisible();
+
+    await page.locator(".input").fill("Third task to mark as done");
+    await page.keyboard.press("Enter");
+    const task3 = page.locator(".task", {
+      hasText: "Third task to mark as done",
+    });
+    await expect(task3).toBeVisible();
+
+    await page.locator(".input").fill("Fourth task to mark as done");
+    await page.keyboard.press("Enter");
+    const task4 = page.locator(".task", {
+      hasText: "Fourth task to mark as done",
+    });
+    await expect(task4).toBeVisible();
+
+    await page.locator(".input").fill("Fifth task to mark as done");
+    await page.keyboard.press("Enter");
+    const task5 = page.locator(".task", {
+      hasText: "Fifth task to mark as done",
+    });
+    await expect(task5).toBeVisible();
+
+    await expect(page.locator(".task").nth(0)).toContainText(
+      "Fifth task to mark as done"
+    );
+    await expect(page.locator(".task").nth(1)).toContainText(
+      "Fourth task to mark as done"
+    );
+    await expect(page.locator(".task").nth(2)).toContainText(
+      "Third task to mark as done"
+    );
+    await expect(page.locator(".task").nth(3)).toContainText(
+      "Second task to mark as done"
+    );
+    await expect(page.locator(".task").nth(4)).toContainText(
+      "First task to mark as done"
+    );
+
+    const tasks = await page.locator(".task").all();
+    for (const task of tasks) {
+      await task.click();
+    }
+
+    for (const task of tasks) {
+      await expect(task).toHaveCSS("text-decoration", /line-through/);
+    }
+
+    
+    const removeDoneButton = page.locator(
+        'button:has-text("Remove Done Todos")'
+    );
+    
+    await expect(removeDoneButton).toBeVisible();
+    
+    await page.locator("text=ACTIVE").click()
+
+    await expect(removeDoneButton).toBeVisible();
+
+    await removeDoneButton.click();
+
+    await expect(page.locator(".task")).toHaveCount(0);
+
+
+  })
 });
